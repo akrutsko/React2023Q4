ce1e70c7bcac97628dc87b8e831a64c61e1d73ff
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-10-26 21:56:28 +0300

    feat: implement App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,57 @@
 import { Component } from 'react';
 import './App.css';
+import { Search } from './components/search/Search';
+import { Result } from './components/result/Result';
+import { Data, Person, ResourcesType } from './interfaces/SWApi';
 
-class App extends Component {
+const URL = `https://swapi.dev/api/${ResourcesType.People}/`;
+
+type AppState = {
+  searchTerm: string;
+  isLoading: boolean;
+  data: Person[];
+};
+
+class App extends Component<null, AppState> {
+  state = {
+    searchTerm: localStorage.getItem('ak-react-search-term') || '',
+    isLoading: true,
+    data: [],
+  };
+
+  fetchData = (searchTerm: string = '') => {
+    this.setState({ isLoading: true });
+
+    const searchParam = searchTerm && `?search=${searchTerm}`;
+
+    fetch(URL + searchParam)
+      .then((res) => res.json())
+      .then((data: Data<Person>) => {
+        this.setState({ data: data.results, isLoading: false });
+      })
+      .catch(() => this.setState({ isLoading: false }));
+  };
+
+  handleClick = (searchTerm: string) => {
+    this.setState({ searchTerm });
+    localStorage.setItem('ak-react-search-term', searchTerm);
+    this.fetchData(searchTerm);
+  };
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.fetchData(searchTerm);
+  }
+
   render() {
-    return null;
+    const { searchTerm, isLoading, data } = this.state;
+
+    return (
+      <main>
+        <Search onClick={this.handleClick} searchTerm={searchTerm} />
+        <Result isLoading={isLoading} data={data} />
+      </main>
+    );
   }
 }
 
