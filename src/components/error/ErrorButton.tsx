cfb99bf8bda2e53e38d118bad2f20757ee09bd34
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-10-27 23:09:53 +0300

    fix: add error button state type

--- src/components/error/ErrorButton.tsx
+++ src/components/error/ErrorButton.tsx
@@ -1,6 +1,10 @@
 import { Component } from 'react';
 
-export class ErrorButton extends Component {
+type ErrorButtonState = {
+  throwError: boolean;
+};
+
+export class ErrorButton extends Component<unknown, ErrorButtonState> {
   state = {
     throwError: false,
   };
