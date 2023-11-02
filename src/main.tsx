af8acdc6048e879cfb02ba05f31e0ca4bd118a2f
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-11-02 17:52:51 +0300

    feat: add react router

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App.tsx';
 import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary.tsx';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />}></Route>),
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <RouterProvider router={router} />
     </ErrorBoundary>
   </React.StrictMode>,
 );
