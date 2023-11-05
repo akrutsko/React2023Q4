c481449bcb62e0762eb9de55cf683d155b6cbc5a
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-11-05 14:51:06 +0300

    feat: add NoPerson data fallback component

--- src/components/NoResults/NoResults.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './NoResults.module.css';
-
-function NoResults() {
-  return <p className={styles.title}>No results found</p>;
-}
-
-export default NoResults;
---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,9 @@
+import noPerson from '../../assets/images/no-person.png';
+
+export default function NoResults() {
+  return (
+    <div>
+      <img src={noPerson} alt="no person data" />
+    </div>
+  );
+}
