7f156741d9fc5d4c5f3f024d7a42025111685859
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-11-05 23:21:09 +0300

    refactor: extract router from main

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,8 @@
 import './index.css';
 
 import ReactDOM from 'react-dom/client';
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from 'react-router-dom';
-import App from './components/App/App.tsx';
-import NotFound from './components/NotFound/NotFound.tsx';
-import ResultDetails from './components/Results/ResultDetails/ResultDetails.tsx';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route
-        path=":id"
-        element={<ResultDetails />}
-        errorElement={<NotFound />}
-      />
-    </Route>,
-  ),
-);
+import { RouterProvider } from 'react-router-dom';
+import { router } from './components/Router/router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <RouterProvider router={router} />,
