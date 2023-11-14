16d6c8c52fb1645cd067f91551429317600b1208
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-11-14 17:50:30 +0300

    refactor: update Search component to use store

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,18 +1,15 @@
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useSearchParams } from 'react-router-dom';
-import { AppDispatch, RootState } from '../../app/store';
-import { setSearch } from '../../features';
+import { useSelector } from 'react-redux';
+import { INIT_PAGE } from '../../app/constants/constants';
+import { RootState } from '../../app/store';
 import { useFetchPersons } from '../../hooks';
 import Pagination from '../Pagination/Pagination';
 import Results from '../Results/Results';
 import Search from '../Search/Search';
-import { INIT_PAGE, SEARCH_PARAM_PAGE } from '../../app/constants/constants';
 
 export default function Main() {
   const searchTerm = useSelector((state: RootState) => state.search.searchTerm);
   const limit = useSelector((state: RootState) => state.itemsPerPage.limit);
-  const dispatch = useDispatch<AppDispatch>();
 
   const [currentPage, setCurrentPage] = useState(INIT_PAGE);
   const [isLoading, totalResults] = useFetchPersons(
@@ -20,18 +17,10 @@ export default function Main() {
     currentPage,
     limit,
   );
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const handleSearch = (search: string) => {
-    dispatch(setSearch(search));
-    setCurrentPage(INIT_PAGE);
-    searchParams.delete(SEARCH_PARAM_PAGE);
-    setSearchParams(searchParams);
-  };
 
   return (
     <main>
-      <Search onSearch={handleSearch} />
+      <Search setPage={setCurrentPage} />
       <Results isLoading={isLoading}>
         <Pagination
           currentPage={currentPage}
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,29 +1,36 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import logo from '../../assets/images/star-wars.png';
 import styles from './Search.module.css';
 
-import { useRef } from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../app/store';
+import { Dispatch, SetStateAction, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../app/store';
+import { INIT_PAGE, SEARCH_PARAM_PAGE } from '../../app/constants/constants';
+import { setSearch } from '../../features';
 
 type Props = {
-  onSearch: (searchTerm: string) => void;
+  setPage: Dispatch<SetStateAction<number>>;
 };
 
-export default function Search({ onSearch }: Props) {
-  const searchTerm = useSelector((state: RootState) => state.search.searchTerm);
+export default function Search({ setPage }: Props) {
   const textInput = useRef<HTMLInputElement>(null);
+  const searchTerm = useSelector((state: RootState) => state.search.searchTerm);
+  const dispatch = useDispatch<AppDispatch>();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const handleSearchClick = () => {
     const value = textInput.current?.value.trim() || '';
-    onSearch(value);
+    dispatch(setSearch(value));
+    setPage(INIT_PAGE);
+    searchParams.delete(SEARCH_PARAM_PAGE);
+    setSearchParams(searchParams);
   };
 
   const handleLogoClick = () => {
     if (textInput.current) {
       textInput.current.value = '';
     }
-    onSearch('');
+    handleSearchClick();
   };
 
   return (
