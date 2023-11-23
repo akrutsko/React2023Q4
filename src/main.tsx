af40729d1c76aca5839525e420321b3a805e19f4
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-11-23 18:02:52 +0300

    fix: remove app

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import './index.css';
-
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router/router';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />,
-);
