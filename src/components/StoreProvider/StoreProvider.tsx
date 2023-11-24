a367dcedc0a53aa64fb924048d556eb5ff403b8f
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-11-24 16:16:45 +0300

    refactor: update to get rid of legacy code

---
+++ src/components/StoreProvider/StoreProvider.tsx
@@ -0,0 +1,8 @@
+import { wrapper } from '@/src/store/store';
+import { PropsWithChildren } from 'react';
+import { Provider } from 'react-redux';
+
+export default function StoreProvider({ children }: PropsWithChildren) {
+  const { store } = wrapper.useWrappedStore({});
+  return <Provider store={store}>{children}</Provider>;
+}
