17594452693ed16279ecc0b2756cb0ad0f4eb9c2
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-11-26 20:07:36 +0300

    chore: remove vite, add vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,7 @@
 /// <reference types="vitest" />
-/// <reference types="vite/client" />
 
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
